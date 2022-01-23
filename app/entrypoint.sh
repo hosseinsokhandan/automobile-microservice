d71a3b5de4f271736a8602612f37b3c927faaa44
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-23 18:13:18 +0330

    wellformatted.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-if [ "$DB_SCHEME" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+if [ "$DB_SCHEME" = "postgres" ]; then
+  echo "Waiting for postgres..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
+  while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+  done
 
-    echo "PostgreSQL started"
+  echo "PostgreSQL started"
 fi
-
 
 aerich upgrade
 
