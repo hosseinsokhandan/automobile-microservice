f52d5d54b47453de0588bcd6d4dd970d3f785368
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-23 19:52:34 +0330

    register_tortoise

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-import asyncio
 from fastapi import FastAPI
 from api import router
-from httpx import AsyncClient
 from settings import get_settings
+from tortoise.contrib.fastapi import register_tortoise
+from database import TORTOISE_ORM
 
 settings = get_settings()
 app = FastAPI()
 
 
-app.include_router(router, prefix="")
+app.include_router(router, prefix="/automobile")
+
+register_tortoise(
+    app,
+    config=TORTOISE_ORM,
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
