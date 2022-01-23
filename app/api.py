b164507bc69f6b7b0f4d51a596dae522958ce369
Hossein Sokhandan <iamsokhandan@gmail.com>
2022-01-23 19:52:40 +0330

    populate

--- app/api.py
+++ app/api.py
@@ -1,11 +1,37 @@
 from fastapi import APIRouter
-from schemas import AutomobileOutSchema
+from schemas import AutomobileInSchema, AutomobileOutSchema
 from services import automobile_service
 
 
 router = APIRouter()
 
 
+@router.get("/populate")
+async def generate_data():
+    await automobile_service._delete_all()
+    await automobile_service.create(
+        AutomobileInSchema(
+            name="Camaro SS", type="SEDAN", model="2019",
+            manufacturer="Chevrolet"
+        )
+    )
+    await automobile_service.create(
+        AutomobileInSchema(
+            name="Ford GT", type="SPORT", model="2006",
+            manufacturer="Ford Motor Company"
+        )
+    )
+    await automobile_service.create(
+        AutomobileInSchema(
+            name="Mustang", type="COUPE", model="2017",
+            manufacturer="Ford Motor Company"
+        )
+    )
+    return {"message": "Populated."}
+
 @router.get("/{id}", response_model=AutomobileOutSchema)
-def get(id: int):
-    return automobile_service.get(id)
+async def get(id: int):
+    return await automobile_service.get(id)
+
+
+
--- app/services.py
+++ app/services.py
@@ -1,9 +1,16 @@
+from schemas import AutomobileInSchema
 from models import AutomobileModel
 
 
 class AutomobileService:
-    async def get(self, id: int):
+    async def get(self, id: int) -> AutomobileModel:
         return await AutomobileModel.get(id=id)
+
+    async def create(self, automobile: AutomobileInSchema):
+        return await AutomobileModel.create(**dict(automobile))
+
+    async def _delete_all(self):
+        return await AutomobileModel.all().delete()
 
 
 automobile_service = AutomobileService()
